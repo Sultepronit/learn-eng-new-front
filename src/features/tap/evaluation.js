35febc156be40c57d207d699f4bf2d829db3e5d3
Sultepronit <sultepronit@gmail.com>
2024-10-13 21:44:08 +0300

    implemented basic version of evaluation

--- src/features/tap/evaluation.js
+++ src/features/tap/evaluation.js
@@ -1,6 +1,14 @@
-import { marks } from "./statuses";
+import { marks, stages } from "./statuses";
 
-function commonEvaluation(card, mark, progressUpdate) {
+function tryAndDegrade(card, progressUpdate) {
+    card.repeatStatus = 0;
+    card.tapFProgress = 0;
+    card.tapBProgress = 0;
+
+    progressUpdate.push('degrade');
+}
+
+function updateProgress(card, mark, progressUpdate) {
     if (mark === marks.GOOD) {
         card[`tap${card.direction}Progress`] = 1;
         progressUpdate.push('good');
@@ -10,13 +18,30 @@ function commonEvaluation(card, mark, progressUpdate) {
     }
 }
 
+function tryAndUpgrade(card, progressUpdate) {
+    if (card.tapFProgress > 0 && card.tapBProgress > 0) {
+        card.tapFProgress = 0;
+        card.tapBProgress = 0;
+
+        card.repeatStatus = card.repeatStage === stages.LEARN ? 1 
+            : card.repeatStage === stages.CONFIRM ? 2 : 8888;
+
+        progressUpdate.push('upgrade');
+    }
+}
+
 export default function evaluate(inputCard, mark) {
     const card = { ...inputCard };
     const freezed = { ...inputCard };
 
     const progressUpdate = [];
 
-    commonEvaluation(card, mark, progressUpdate); 
+    if (mark === marks.BAD) {
+        tryAndDegrade(card, progressUpdate);
+    } else {
+        updateProgress(card, mark, progressUpdate); 
+        tryAndUpgrade(card, progressUpdate);
+    }
 
     const changes = {};
     for (const key in card) {
